1a1d2a71a749d8b2e4c3106d4ce46ecb67b09e8f
/Jeronimo <jeronimo_espala@hotmail.com>
2022-01-15 17:30:17 -0300

    DIO BANCO

---
+++ src/com/dio/banco/Bank.java
@@ -0,0 +1,7 @@
+package com.dio.banco;
+
+public class Bank {
+    public static void main(String[] args) {
+        System.out.println("Teste app");
+    }
+}
---
+++ src/com/dio/banco/ContaCorrente.java
@@ -0,0 +1,11 @@
+package com.dio.banco;
+
+public class ContaCorrente extends Conta{
+
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+
+}
